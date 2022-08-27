0c5634571489d360e298c48ffc1b5efcb5ea4b6e
eftaltekerler <eftal.tekerler@yahoo.com>
2022-08-28 00:05:06 +0300

    Initial commit

---
+++ src/ikininkuveti.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ikininkuveti {
+    public static void main(String[] args) {
+        int n ;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Sınır Sayısını Giriniz");
+        n = input.nextInt();
+        for ( int i = 1; i<=100 ; i*=4){
+            System.out.println(i);1
+        }
+    }
+}
